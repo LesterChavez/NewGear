f28c376c86885b780fb6c540a53fb7b61506f85f
Lester Chavez <lchavez9669@conestogac.on.ca>
2017-09-28 18:56:07 -0400

    third commit .. both orientations are working.

--- app/src/main/java/com/example/tysonlove/newgear/RegisterActivity.java
+++ app/src/main/java/com/example/tysonlove/newgear/RegisterActivity.java
@@ -11,7 +11,7 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-     //   final CheckBox
+        //final CheckBox checkBox
 
         //asdfads
 
