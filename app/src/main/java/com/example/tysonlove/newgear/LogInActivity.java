b7a31ee0b803d06fd456dd2bee65d8243bee8610
Lester Chavez <lchavez9669@conestogac.on.ca>
2017-09-29 22:18:04 -0400

    sixth commit
    all criteria for assignment has been met, but it is pretty ugly.

--- app/src/main/java/com/example/tysonlove/newgear/LogInActivity.java
+++ app/src/main/java/com/example/tysonlove/newgear/LogInActivity.java
@@ -1,7 +1,14 @@
 package com.example.tysonlove.newgear;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -9,5 +16,26 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        Button btnSubmitLogIn = (Button)findViewById(R.id.btnLogIn);
+        final EditText txtEmailAddress = (EditText)findViewById(R.id.txtEmailLogIn);
+
+        btnSubmitLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               if (txtEmailAddress.getText().length() > 0)
+               {
+                   Toast.makeText(getApplicationContext(), "You have successfully registered", Toast.LENGTH_LONG).show();
+               }
+
+
+
+                Intent logInIntent = new Intent(LogInActivity.this,StoreActivity.class);
+
+                LogInActivity.this.startActivity(logInIntent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/tysonlove/newgear/RegisterActivity.java
+++ app/src/main/java/com/example/tysonlove/newgear/RegisterActivity.java
@@ -1,19 +1,50 @@
 package com.example.tysonlove.newgear;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.view.View;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+
+
+public class RegisterActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-      //  final CheckBox checkBox
+         final CheckBox checkBox = (CheckBox)findViewById(R.id.chBox);
+         checkBox.setChecked(false);
+         boolean checkBoxState = checkBox.isChecked();
 
-        //asdfads
+
+         Button btnSubmit = (Button)findViewById(R.id.btnRegisterSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (checkBox.isChecked())
+                    Toast.makeText(getApplicationContext(), "You have successfully registered", Toast.LENGTH_LONG).show();
+
+
+
+                Intent btnSubmitIntent = new Intent(RegisterActivity.this,StoreActivity.class);
+
+
+
+                RegisterActivity.this.startActivity(btnSubmitIntent);
+
+
+
+            }
+        });
+
+
 
     }
 }
