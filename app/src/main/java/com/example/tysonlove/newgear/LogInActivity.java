e532fe4ad0d5a5bbae4a60e2fe0caa3e5e5790f9
Lester Chavez <lchavez9669@conestogac.on.ca>
2017-10-18 00:16:29 -0400

    seventh commit
    Log in page validation completed

--- app/src/main/java/com/example/tysonlove/newgear/LogInActivity.java
+++ app/src/main/java/com/example/tysonlove/newgear/LogInActivity.java
@@ -3,6 +3,7 @@ package com.example.tysonlove.newgear;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -19,19 +20,26 @@ public class LogInActivity extends AppCompatActivity {
 
         Button btnSubmitLogIn = (Button) findViewById(R.id.btnLogIn);
         final EditText txtEmailAddress = (EditText) findViewById(R.id.txtEmailLogIn);
+        final EditText txtPasswordLogIn = (EditText) findViewById(R.id.txtPasswordLogIn);
 
         btnSubmitLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (txtEmailAddress.getText().toString().length() < 0 || !Patterns.EMAIL_ADDRESS.matcher(txtEmailAddress.getText().toString()).matches()) {
 
-                if (txtEmailAddress.getText().length() > 0) {
-                    Toast.makeText(getApplicationContext(), "You have successfully logged in", Toast.LENGTH_LONG).show();
+                    txtEmailAddress.setError("Format must be something@something.something");
                 }
 
 
-                Intent logInIntent = new Intent(LogInActivity.this, StoreActivity.class);
+                if (txtPasswordLogIn.getText().toString().length() == 0) {
+                    txtPasswordLogIn.setError("Password must not be blank");
+                } else {
+                    Intent logInIntent = new Intent(LogInActivity.this, StoreActivity.class);
 
-                LogInActivity.this.startActivity(logInIntent);
+                    LogInActivity.this.startActivity(logInIntent);
+                }
+
+
             }
         });
 
