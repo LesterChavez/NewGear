637fe04e62962743fcc53650da0b5d868bab13ea
Lester Chavez <lchavez9669@conestogac.on.ca>
2017-10-23 14:24:06 -0400

    8th commit
    login and register validations completed

--- app/src/main/java/com/example/tysonlove/newgear/LogInActivity.java
+++ app/src/main/java/com/example/tysonlove/newgear/LogInActivity.java
@@ -5,43 +5,115 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.util.Log;
 
 
 public class LogInActivity extends AppCompatActivity {
+    //    final Button btnSubmitLogIn = (Button) findViewById(R.id.btnLogIn);
+//    final EditText txtEmailAddress = (EditText) findViewById(R.id.txtEmailLogIn);
+//    final EditText txtPasswordLogIn = (EditText) findViewById(R.id.txtPasswordLogIn);
+    // public int counter = 0;
+    private final static String Tag = "validateMeth";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-        Button btnSubmitLogIn = (Button) findViewById(R.id.btnLogIn);
+        final Button btnSubmitLogIn = (Button) findViewById(R.id.btnLogIn);
         final EditText txtEmailAddress = (EditText) findViewById(R.id.txtEmailLogIn);
         final EditText txtPasswordLogIn = (EditText) findViewById(R.id.txtPasswordLogIn);
+
 
         btnSubmitLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (txtEmailAddress.getText().toString().length() < 0 || !Patterns.EMAIL_ADDRESS.matcher(txtEmailAddress.getText().toString()).matches()) {
 
-                    txtEmailAddress.setError("Format must be something@something.something");
-                }
+                // validateMeth();
+                if (validateMeth()) {
+                    Log.d(Tag, "meth on call if true?? ");
 
-
-                if (txtPasswordLogIn.getText().toString().length() == 0) {
-                    txtPasswordLogIn.setError("Password must not be blank");
-                } else {
                     Intent logInIntent = new Intent(LogInActivity.this, StoreActivity.class);
 
                     LogInActivity.this.startActivity(logInIntent);
+                } else {
+                    Log.d(Tag, "meth on call if false?? ");
+
+                    Toast.makeText(getApplicationContext(), "Error all fields must be entered", Toast.LENGTH_LONG).show();
+
+
+//
                 }
 
 
             }
+
+            private boolean validateMeth() {
+                int counter = 0;
+                if (txtEmailAddress.getText().toString().trim().length() < 0 || !Patterns.EMAIL_ADDRESS.matcher(txtEmailAddress.getText().toString().trim()).matches()) {
+                    txtEmailAddress.setError("Format must be something@something.something");
+
+                    counter++;
+                    Log.d(Tag, "counter in email is " + counter);
+                }
+
+                if (txtPasswordLogIn.getText().toString().length() == 0) {
+                    txtPasswordLogIn.setError("Password must not be blank");
+                    counter++;
+                    Log.d(Tag, "counter in pass is " + counter);
+
+                    //  btnSubmitLogIn.setEnabled(false);
+                    //  btnSubmitLogIn.setEnabled(false);
+                }
+                if (counter > 0) {
+                    Log.d(Tag, "returned false, counter is  " + counter);
+
+                    return false;
+
+
+                } else {
+
+                    counter = 0;
+                    Log.d(Tag, "returned true, counter is  " + counter);
+
+                    return true;
+                }
+
+
+            }
+
         });
 
+
     }
+
+//    private boolean validateMeth() {
+//
+//        if (txtEmailAddress.getText().toString().trim().length() < 0 || !Patterns.EMAIL_ADDRESS.matcher(txtEmailAddress.getText().toString().trim()).matches()) {
+//            txtEmailAddress.setError("Format must be something@something.something");
+//            counter = 0;
+//            counter++;
+//        }
+//
+//        if (txtPasswordLogIn.getText().toString().length() == 0) {
+//            txtPasswordLogIn.setError("Password must not be blank");
+//            counter++;
+//            //  btnSubmitLogIn.setEnabled(false);
+//            //  btnSubmitLogIn.setEnabled(false);
+//        }
+//        if (counter >= 1) {
+//            return false;
+//        } else {
+//            return true;
+//        }
+//
+//
+//    }
 }
+
+
--- app/src/main/java/com/example/tysonlove/newgear/RegisterActivity.java
+++ app/src/main/java/com/example/tysonlove/newgear/RegisterActivity.java
@@ -3,13 +3,18 @@ package com.example.tysonlove.newgear;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
 public class RegisterActivity extends AppCompatActivity {
+    private final static String Tag = "validateMethRegClass";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +23,14 @@ public class RegisterActivity extends AppCompatActivity {
 
         final CheckBox checkBox = (CheckBox) findViewById(R.id.chBox);
         checkBox.setChecked(false);
-        boolean checkBoxState = checkBox.isChecked();
+        final EditText fName = (EditText) findViewById(R.id.txtFirstName);
+        final EditText lName = (EditText) findViewById(R.id.txtLastName);
+        final EditText pWord = (EditText) findViewById(R.id.txtPassword);
+        final EditText phNum = (EditText) findViewById(R.id.txtPhoneNumber);
+        final EditText eMail = (EditText) findViewById(R.id.txtEmailAddress);
+
+
+        final boolean checkBoxState = checkBox.isChecked();
 
 
         Button btnSubmit = (Button) findViewById(R.id.btnRegisterSubmit);
@@ -27,19 +39,72 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                if (checkBox.isChecked())
-                    Toast.makeText(getApplicationContext(), "You have successfully registered", Toast.LENGTH_LONG).show();
+                if (validateMethRegClass()) {
+                    Log.d(Tag, "meth on call if true?? ");
+
+                    Intent logInIntent = new Intent(RegisterActivity.this, StoreActivity.class);
+
+                    RegisterActivity.this.startActivity(logInIntent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error all fields must be entered", Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+
+            private boolean validateMethRegClass() {
+                int counter = 0;
+                if (fName.getText().toString().trim().isEmpty()) {
+
+                    fName.setError("fist name must not be blank");
+                    counter++;
+                    Log.d(Tag, "fname blank " + counter);
+
+                }
+                if (lName.getText().toString().trim().isEmpty()) {
+                    lName.setError("last name must not be blank");
+                    counter++;
+                    Log.d(Tag, "lname blank " + counter);
+
+                }
+                if (pWord.getText().toString().trim().isEmpty()) {
+                    pWord.setError("Password must not be blank");
+                    counter++;
+                    Log.d(Tag, "pWord blank " + counter);
+
+                }
+                if (phNum.getText().toString().trim().isEmpty()) {
+                    phNum.setError("Name must not be blank");
+                    counter++;
+                    Log.d(Tag, "ph# blank " + counter);
+
+                }
+                if (eMail.getText().toString().trim().isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(eMail.getText().toString().trim()).matches()) {
+                    eMail.setError("Format must be something@something.something");
+                    counter++;
+                    Log.d(Tag, "Email blank " + counter);
+
+                }
+                if (!checkBox.isChecked()) {
+                    // Toast.makeText(getApplicationContext(), "Come back when you are over 18", Toast.LENGTH_LONG).show();
+                    //  Intent btnSubmitIntent = new Intent(RegisterActivity.this, StoreActivity.class);
+                    counter++;
+                    Log.d(Tag, "ch Box blank " + counter);
 
 
-                Intent btnSubmitIntent = new Intent(RegisterActivity.this, StoreActivity.class);
+                }
+                if (counter > 0) {
+                    return false;
+                } else {
+                    return true;
 
-
-                RegisterActivity.this.startActivity(btnSubmitIntent);
-
+                }
 
             }
         });
 
 
     }
+
+
 }
